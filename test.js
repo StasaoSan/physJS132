d362653ef44617b4ec46648931a62fabf4ba9264
StasaoSan <tadphp@yandex.ru>
2023-12-19 00:16:53 +0300

    chore
    edited after review

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const chartInstances = {};
     const toggleThemeButton = document.getElementById('toggleThemeButton');
+    const showExamplesButton = document.getElementById('showExamplesButton');
+    const exampleList = document.getElementById('exampleList');
 
     function clearCanvas(canvasId) {
         const canvas = document.getElementById(canvasId);
@@ -50,8 +52,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function integrateFunction(func, variable, start, end, step) {
         let totalArea = 0;
         for (let x = start; x < end; x += step) {
-            let valueAtX = math.evaluate(func.replace(new RegExp(variable, 'g'), `(${x})`));
-            let valueAtNextX = math.evaluate(func.replace(new RegExp(variable, 'g'), `(${x + step})`));
+
+            let functionAtX = func.replace(new RegExp(variable, 'g'), `(${x})`).replace(/log\((.*?), (.*?)\)/g, (match, number, base) =>
+                `math.log(${number}, ${base})`
+            );
+            let functionAtNextX = func.replace(new RegExp(variable, 'g'), `(${x + step})`).replace(/log\((.*?), (.*?)\)/g, (match, number, base) =>
+                `math.log(${number}, ${base})`
+            );
+
+            let valueAtX = math.evaluate(functionAtX);
+            let valueAtNextX = math.evaluate(functionAtNextX);
             let averageHeight = (valueAtX + valueAtNextX) / 2;
             totalArea += averageHeight * step;
         }
@@ -66,35 +76,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
             for (let i = 0; i < derivativeOrder; i++) {
                 functionToEvaluate = math.derivative(functionToEvaluate, 't').toString();
             }
-            for (let t = 0; t <= 10; t += 0.1) {
-                let value = math.evaluate(functionToEvaluate.replace(/t/g, `(${t})`));
-                values.push(parseFloat(value.toFixed(2)));
-            }
-        } else if (derivativeOrder === -1) {
-            let integralValue = 0;
-            for (let t = 0; t <= 10; t += 0.1) {
-                integralValue += integrateFunction(functionToEvaluate, 't', t, t + 0.1, 0.01);
+        }
+
+        for (let t = 0; t <= 10; t += 0.1) {
+            let evaluatedFunction = functionToEvaluate.replace(/t/g, `(${t})`);
+
+            evaluatedFunction = evaluatedFunction.replace(/log\((.*?), (.*?)\)/g, (match, number, base) =>
+                `math.log(${number}, ${base})`
+            );
+
+            if (derivativeOrder === -1 || derivativeOrder === -2) {
+                let integralValue = integrateFunction(evaluatedFunction, 't', 0, t, 0.01);
+                if (derivativeOrder === -2) {
+                    integralValue = integrateFunction(integralValue.toString(), 't', 0, t, 0.01);
+                }
                 values.push(parseFloat(integralValue.toFixed(2)));
-            }
-        } else if (derivativeOrder === -2) {
-            let integralVelocity = 0;
-            let integralPosition = 0;
-            for (let t = 0; t <= 10; t += 0.1) {
-                integralVelocity += integrateFunction(userFunction, 't', t, t + 0.1, 0.01);
-                integralPosition += integralVelocity * 0.1;
-                values.push(parseFloat(integralPosition.toFixed(2)));
-            }
-        } else {
-            for (let t = 0; t <= 10; t += 0.1) {
-                let value = math.evaluate(functionToEvaluate.replace(/t/g, `(${t})`));
+            } else {
+                let value = math.evaluate(evaluatedFunction);
                 values.push(parseFloat(value.toFixed(2)));
             }
         }
 
         return values;
     }
-
-
 
     function determineAndDrawGraphs() {
         let userInput = document.getElementById('functionInput').value;
@@ -111,14 +115,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     drawGraph('sideGraph2', 'Ускорение от времени', xValues, calculateValues(userFunction, 2), 'green', 'Время (с)', 'Ускорение (м/с²)');
                     break;
                 case 'v':
-                    drawGraph('mainGraph', 'Скорость от времени', xValues, calculateValues(userFunction, 1), 'red', 'Время (с)', 'Скорость (м/с)');
-                    drawGraph('sideGraph1', 'Координата от времени', xValues, calculateValues(userFunction), 'blue', 'Время (с)', 'Координата (м)');
-                    drawGraph('sideGraph2', 'Ускорение от времени', xValues, calculateValues(userFunction, 2), 'green', 'Время (с)', 'Ускорение (м/с²)');
+                    drawGraph('mainGraph', 'Скорость от времени', xValues, calculateValues(userFunction), 'red', 'Время (с)', 'Скорость (м/с)');
+                    drawGraph('sideGraph1', 'Координата от времени', xValues, calculateValues(userFunction, -1), 'blue', 'Время (с)', 'Координата (м)');
+                    drawGraph('sideGraph2', 'Ускорение от времени', xValues, calculateValues(userFunction, 1), 'green', 'Время (с)', 'Ускорение (м/с²)');
                     break;
                 case 'a':
-                    drawGraph('mainGraph', 'Ускорение от времени', xValues, calculateValues(userFunction, 2), 'green', 'Время (с)', 'Ускорение (м/с²)');
-                    drawGraph('sideGraph1', 'Координата от времени', xValues, calculateValues(userFunction), 'blue', 'Время (с)', 'Координата (м)');
-                    drawGraph('sideGraph2', 'Скорость от времени', xValues, calculateValues(userFunction, 1), 'red', 'Время (с)', 'Скорость (м/с)');
+                    drawGraph('mainGraph', 'Ускорение от времени', xValues, calculateValues(userFunction), 'green', 'Время (с)', 'Ускорение (м/с²)');
+                    drawGraph('sideGraph1', 'Скорость от времени', xValues, calculateValues(userFunction, -1), 'red', 'Время (с)', 'Скорость (м/с)');
+                    drawGraph('sideGraph2', 'Координата от времени', xValues, calculateValues(userFunction, -2), 'blue', 'Время (с)', 'Координата (м)');
                     break;
                 default:
                     alert('Введите корректную функцию (x(t), v(t) или a(t))');
@@ -128,9 +132,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+
     document.getElementById('drawGraphsButton').addEventListener('click', determineAndDrawGraphs);
 
     toggleThemeButton.addEventListener('click', () => {
         document.body.classList.toggle('night-theme');
+    });
+
+    showExamplesButton.addEventListener('click', () => {
+        exampleList.style.display = exampleList.style.display === 'none' ? 'block' : 'none';
     });
 });
